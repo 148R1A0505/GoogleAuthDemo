d0ab16919d3ea6804568a5826dcaee0d53c754d5
Sumanth Akkala <sumanth.akkala@ggktech.local>
2018-03-09 19:55:12 +0530

    2nd

---
+++ GoogleAuthOwn1/GoogleAuthOwn/App_Start/AuthConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetOpenAuth.GoogleOAuth2;
+using Microsoft.AspNet.Membership.OpenAuth;
+
+
+
+namespace GoogleAuthOwn
+{
+    public class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            GoogleOAuth2Client clientGoog = new GoogleOAuth2Client("275657770228-6dele1ri26720stq9f99qpeqrlhuqkth.apps.googleusercontent.com", "Xrnnelu22SiA99grvsWJd7kD");
+            IDictionary<string, string> extraData = new Dictionary<string, string>();
+            OpenAuth.AuthenticationClients.Add("google", () => clientGoog, extraData);
+        }
+    }
+}
---
+++ GoogleAuthOwn1/GoogleAuthOwn/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNet.Membership.OpenAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleAuthOwn.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserProfile objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AuthMVCEntities db = new AuthMVCEntities())
+                {
+                    var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("UserDashBoard");
+                        
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+
+        public ActionResult RedirectToGoogle()
+        {
+            string provider = "google";
+            string returnUrl = "";
+            return new ExternalLoginResult(provider, Url.Action("signin-google", new { ReturnUrl = returnUrl }));
+        }
+        internal class ExternalLoginResult : ActionResult
+        {
+            public ExternalLoginResult(string provider, string returnUrl)
+            {
+                Provider = provider;
+                ReturnUrl = returnUrl;
+            }
+
+            public string Provider { get; private set; }
+            public string ReturnUrl { get; private set; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                OpenAuth.RequestAuthentication(Provider, ReturnUrl);
+            }
+        }
+
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
